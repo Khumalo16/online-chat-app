2a1f5da2199aabad05744e0531888a8cb62897ef
22282289 <22282289@sun.ac.za>
2021-02-24 15:51:20 +0200

    first commit

--- application.py
+++ application.py
@@ -1,24 +1,47 @@
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, flash;
 from flask_socketio import SocketIO, emit, send
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = 'mysecret'
 socketio = SocketIO(app)
-
+users = []
 @app.route("/")
 def index():
     return render_template('flack/index.html')
 
-@socketio.on("submit vote")
-def vote(data):
-    selection = data["selection"]
-    print(selection)
-    emit("announce vote", {"selection": selection}, broadcast=True)
-if __name__ == '__main__':
-    socketio.run(app)
-    
+
 @app.route("/registration")
 def registration():
     return render_template('flack/registration.html')
+
+@socketio.on("username")
+def username(data):
+
+
+    if data["user"]:
+        data["user"] = data["user"].capitalize()
+    if not users and data["user"]:
+        
+        users.append(data['user'])
+    if len(users) > 0:
+      
+        for user in users:
+            if user == data["user"]:
+                data["user"] = "1"
+    
+    if data["user"] and data["user"] != "1":
+        users.append(data["user"])         
+    print(users, "this is not zero")
+    #del users[:]
+    print("this list is empyt", data["user"])
+    emit("username", {"user": data["user"]}, broadcast=True)
+
+@socketio.on("message sent")
+def message():
+    print("the message was received...")
+    emit("message back", broadcast=True)
+
+if __name__ == '__main__':
+    socketio.run(app)
--- static/index.js
+++ static/index.js
@@ -1,53 +1,71 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    // Set local storage
-    
+   //localStorage.clear();
 
-    localStorage.clear();
+
     if (localStorage.getItem("username") === null) {
         //...
         load_page(registration.dataset.registration);
-     
-        console.log("trying")
-            function load_page(name) {
-            const request = new XMLHttpRequest();
-            request.open('GET',`/${name}`);
-            request.onload = () => {
-                const response = request.responseText;
-                document.getElementById('container').innerHTML= response;
-            };
-            request.send();
-        }
-    }
-  
-    var name = document.querySelector('.fo').value;
-    console.log(name);
-   
-    // Connect to websocket
-  
- 
-  /*  var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
-
-    // When connected, configure buttons
-    socket.on('connect', () => {
-
-        // Each button should emit a "submit vote" event
-        document.querySelectorAll('button').forEach(button => {
-            button.onclick = () => {
-                const selection = document.getElementById('text').value;
-                socket.emit('submit vote', {'selection': selection});
-            };
+        document.addEventListener('click', e => {
+                if(e.target && e.target.id == 'btn-primary') {
+                    const user = document.querySelector('.form-control').value;
+                    localStorage.setItem('user', user );
+                }
         });
-    });
+    } 
+   
+   if (localStorage.getItem('user') != null && localStorage.getItem('username') === null) {
+        var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+        socket.on('connect', () => {
+            const user = localStorage.getItem("user");
+            socket.emit('username', {'user': user});
+        });
 
-    // When a new vote is announced, add to the unordered list
-    socket.on('announce vote', data => {
+        socket.on('username', data => {
+            localStorage.setItem("username", data.user);    
+        });
+
+        load_page("");
+        function refresh() {    
+            setTimeout(function () {
+                location.reload()
+            }, 100);
+        }
+       refresh();
+   }
+
+    function load_page(name) {
+        const request = new XMLHttpRequest();
+        request.open('GET',`/${name}`);
+        request.onload = () => {
+            const response = request.responseText;
+            document.getElementById('container').innerHTML= response;
+        };
+        request.send();
+    }
+
+    function message() {
+        var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+        socket.on('connect', () => {
+            document.querySelectorAll('button').forEach(button => {
+                button.onclick = ()=> {
+                    socket.emit('message sent');
+                };
+            })
+           
+        });
+
+        socket.on('message back', () => {
+      
         const li = document.createElement('li');
-        li.innerHTML = `${data.selection}`;
-
-        document.querySelector('#all').append(li);
-    }); */
+        li.innerHTML = document.getElementById('text').value;
+        document.getElementById('all').append(li);
+          
+        });
+    }   
+    
+    message();
 });
 
- 
+
